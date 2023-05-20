0c460efa0efc45cd2ea9fa4fdfd62c23fe9a80de
andrew.maurice <andromulin@gmail.com>
2023-05-20 07:41:42 -0500

    channges

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -10,12 +10,12 @@ import {
 import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDDU4V-_QV3M8GyhC9SVieRTDM4dbiT0Yk',
-  authDomain: 'crwn-clothing-db-98d4d.firebaseapp.com',
-  projectId: 'crwn-clothing-db-98d4d',
-  storageBucket: 'crwn-clothing-db-98d4d.appspot.com',
-  messagingSenderId: '626766232035',
-  appId: '1:626766232035:web:506621582dab103a4d08d6',
+  apiKey: "AIzaSyBUfh2-YoPxeF3egP3xQkf6DRT96npywbw",
+  authDomain: "react-crwn-clothing-udemy.firebaseapp.com",
+  projectId: "react-crwn-clothing-udemy",
+  storageBucket: "react-crwn-clothing-udemy.appspot.com",
+  messagingSenderId: "147093509836",
+  appId: "1:147093509836:web:eff95090b252c2c4651469"
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
